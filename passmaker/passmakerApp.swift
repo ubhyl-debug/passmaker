dd64434e4bc8c262ef88d1d94ea74b3f3cfeae72
marencordtshsg <maren.cordts@unisg.ch>
2024-07-07 10:19:17 +0200

    Initial Commit

---
+++ passmaker/passmakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  passmakerApp.swift
+//  passmaker
+//
+//  Created by Maren Cordts on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct passmakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
